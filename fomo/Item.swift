0f735d505f4386c6fbf547cede1c8e9e4a0b5c9f
Axel <a.axelvc@gmail.com>
2025-11-17 23:35:23 -0600

    Initial Commit

---
+++ fomo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  fomo
+//
+//  Created by Axel on 17/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ fomo/fomoApp.swift
@@ -0,0 +1,32 @@
+//
+//  fomoApp.swift
+//  fomo
+//
+//  Created by Axel on 17/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct fomoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
