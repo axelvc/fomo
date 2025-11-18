2f3f1065a886b6688855a95e0bcb1269e9edc7f2
Axel <a.axelvc@gmail.com>
2025-11-18 03:18:25 -0600

    feat: initial model state

--- fomo/ContentView.swift
+++ fomo/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import FamilyControls
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
@@ -17,9 +18,9 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        EditItemView(item: item)
                     } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        Text(item.name)
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -41,7 +42,7 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(timestamp: Date())
+            let newItem = Item(name: "foo")
             modelContext.insert(newItem)
         }
     }
@@ -51,6 +52,136 @@ struct ContentView: View {
             for index in offsets {
                 modelContext.delete(items[index])
             }
+
+            try! modelContext.save()
+        }
+    }
+}
+
+struct EditItemView: View {
+    @Bindable var item: Item
+    @State private var showAppPicker = false
+
+    var body: some View {
+        Form {
+            TextField("Name", text: $item.name)
+            
+            Button("Blocked apps") {
+                showAppPicker.toggle()
+            }
+            .familyActivityPicker(isPresented: $showAppPicker, selection: $item.apps)
+
+            Picker("Block mode", selection: $item.blockMode) {
+                ForEach(BlockMode.allCases) { mode in
+                    Text(mode.title).tag(mode)
+                }
+            }
+
+            switch item.blockMode {
+            case .timer(let duration):
+                let durationBinding = Binding<Duration>(
+                    get: { duration },
+                    set: { item.blockMode = .timer($0) }
+                )
+
+                EditTimerView(duration: durationBinding)
+            case .schedule(let scheduleWindow):
+                let bindignScheduleWindow = Binding<ScheduleWindow>(
+                    get: { scheduleWindow },
+                    set: { item.blockMode = .schedule($0) }
+                )
+
+                EditScheduleView(scheduleWindow: bindignScheduleWindow)
+            case .limit(let limitConfig):
+                let bindignLimitConfig = Binding<LimitConfig>(
+                    get: { limitConfig },
+                    set: { item.blockMode = .limit($0) }
+                )
+
+                EditLimitView(limitConfig: bindignLimitConfig)
+            case .opens(let opensConfig):
+                let bindingOpensConfig = Binding<OpensConfig>(
+                    get: { opensConfig },
+                    set: { item.blockMode = .opens($0) }
+                )
+                
+                EditOpensView(opensConfig: bindingOpensConfig)
+            }
+
+            Picker("Break mode", selection: $item.breakMode) {
+                ForEach(BreakMode.allCases) { mode in
+                    Text(mode.title).tag(mode)
+                }
+            }
+
+            Toggle("Repeat", isOn: $item.repeatOn)
+            Toggle("Notifications", isOn: $item.notificationOn)
+        }
+        .navigationTitle("Edit Item")
+        .onChange(of: item.blockMode) {
+            print(item.blockMode, item.apps)
+        }
+    }
+}
+
+struct EditTimerView: View {
+    @Binding var duration: Duration
+
+    var body: some View {
+        HStack {
+            Text("Time:")
+            Spacer()
+            Picker("", selection: $duration.hours) {
+                ForEach(0..<24) { n in
+                    Text(n.description)
+                }
+            }
+            .pickerStyle(.menu)
+            Picker("", selection: $duration.minutes) {
+                ForEach(0..<60) { n in
+                    Text(n.description)
+                }
+            }
+            .pickerStyle(.menu)
+        }
+    }
+}
+
+struct EditScheduleView: View {
+    @Binding var scheduleWindow: ScheduleWindow
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            DatePicker("From:", selection: $scheduleWindow.start)
+            DatePicker("To:", selection: $scheduleWindow.end)
+        }
+    }
+}
+
+struct EditLimitView: View {
+    @Binding var limitConfig: LimitConfig
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Focus time")
+            EditTimerView(duration: $limitConfig.focus)
+            Text("Break time")
+            EditTimerView(duration: $limitConfig.breakTime)
+        }
+    }
+}
+
+struct EditOpensView: View {
+    @Binding var opensConfig: OpensConfig
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Stepper(
+                "Opens: \(opensConfig.opens)",
+                value: $opensConfig.opens,
+                in: 0...20
+            )
+            EditTimerView(duration: $opensConfig.allowedPerOpen)
         }
     }
 }
--- fomo/Item.swift
+++ fomo/Item.swift
@@ -7,12 +7,79 @@
 
 import Foundation
 import SwiftData
+import FamilyControls
 
 @Model
 final class Item {
-    var timestamp: Date
+    var name: String
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    var apps: FamilyActivitySelection
+    var blockMode: BlockMode
+    var breakMode: BreakMode
+    
+    var repeatOn: Bool
+    var notificationOn: Bool
+    
+    init(name: String) {
+        self.name = name
+        self.apps = .init()
+        self.blockMode = .timer(.init(hours: 0, minutes: 0))
+        self.breakMode = .relaxed
+        self.repeatOn = false
+        self.notificationOn = false
     }
+}
+
+enum BreakMode: String, Codable, CaseIterable, Identifiable {
+    case relaxed, focused, strict
+    
+    var id: Self { self }
+    var title: String { self.rawValue.capitalized }
+}
+
+enum BlockMode: Codable, CaseIterable, Identifiable, Hashable {
+    case timer(Duration)
+    case schedule(ScheduleWindow)
+    case limit(LimitConfig)
+    case opens(OpensConfig)
+    
+    static var allCases: [BlockMode] {
+        [
+            .timer(.init()),
+            .schedule(.init()),
+            .limit(.init()),
+            .opens(.init())
+        ]
+    }
+    
+    var id: String { title }
+    
+    var title: String {
+        return switch self {
+        case .timer: "Timer"
+        case .schedule: "Schedule"
+        case .limit: "Limit"
+        case .opens: "Opens"
+        }
+    }
+}
+
+struct Duration: Codable, Hashable {
+    var hours: Int = 0
+    var minutes: Int = 0
+}
+
+struct ScheduleWindow: Codable, Hashable {
+    var start: Date = .distantPast
+    var end: Date = .distantFuture
+}
+
+struct LimitConfig: Codable, Hashable {
+    var focus: Duration = .init()
+    var breakTime: Duration = .init()
+}
+
+struct OpensConfig: Codable, Hashable {
+    var opens: Int = 0
+    var allowedPerOpen: Duration = .init()
 }
