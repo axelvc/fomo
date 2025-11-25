ce25f53f9387509387ed255f47dd6b96bf28c1cf
Axel <a.axelvc@gmail.com>
2025-11-25 04:27:12 -0600

    feat: add timer for schedule blocks

--- fomo/ContentView.swift
+++ fomo/ContentView.swift
@@ -74,17 +74,28 @@ struct ItemPreview: View {
 
             switch item.blockMode {
             case .timer:
-                DurationTimer()
+                Capsule()
+                    .fill(.gray.tertiary)
+                    .frame(width: 70, height: 20)
+                    .overlay {
+                        DurationTimer(to: item.scheduleWindow.end).font(.caption)
+                    }
             case .schedule:
-                let from = item.scheduleWindow.start.formatted(.dateTime.hour().minute())
-                let to = item.scheduleWindow.end.formatted(.dateTime.hour().minute())
+                Capsule()
+                    .fill(.gray.tertiary)
+                    .frame(width: 85, height: 20)
+                    .overlay {
+                        let from = item.scheduleWindow.start.formatted(.dateTime.hour().minute())
+                        let to = item.scheduleWindow.end.formatted(.dateTime.hour().minute())
+
+                        Text("\(from) - \(to)").font(.caption)
+                    }
 
                 Capsule()
                     .fill(.gray.tertiary)
-                    .frame(maxWidth: 90)
+                    .frame(width: 135, height: 20)
                     .overlay {
-                        Text("\(from) - \(to)")
-                            .font(.caption)
+                        CalendarTimer().font(.caption)
                     }
             case .limit:
                 let freeTime = formattedDuration(item.limitConfig.freeTime)
@@ -133,17 +144,37 @@ struct ItemPreview: View {
     }
 
     @ViewBuilder
-    private func DurationTimer() -> some View {
+    private func DurationTimer(to: Date) -> some View {
         TimelineView(.periodic(from: .now, by: 1)) { context in
-            let remaining = max(0, Int(item.scheduleWindow.end.timeIntervalSince(context.date)))
+            let remaining = max(0, to.timeIntervalSince(context.date))
 
-            let hours = remaining / 3600
-            let minutes = (remaining % 3600) / 60
-            let seconds = remaining % 60
-
-            Text(String(format: "%01d:%02d:%02d", hours, minutes, seconds))
-                .monospacedDigit()
+            Text(intervalToHMS(interval: remaining)).monospacedDigit()
         }
+    }
+
+    @ViewBuilder
+    private func CalendarTimer() -> some View {
+        let start = item.scheduleWindow.start
+        let end = item.scheduleWindow.end
+
+        TimelineView(.periodic(from: .now, by: 1)) { context in
+            let isRunning = start < context.date && context.date < end
+            let interval = max(0, (isRunning ? end : start).timeIntervalSince(context.date))
+
+            HStack(spacing: 0) {
+                Text(isRunning ? "Running: " : "Starting in: ")
+                Text(intervalToHMS(interval: interval)).monospacedDigit()
+            }
+        }
+    }
+
+    private func intervalToHMS(interval: TimeInterval) -> String {
+        let total = Int(interval)
+        let hours = total / 3600
+        let minutes = (total % 3600) / 60
+        let seconds = total % 60
+
+        return String(format: "%01d:%02d:%02d", hours, minutes, seconds)
     }
 
     private func formattedDuration(_ duration: Duration) -> String {
@@ -162,6 +193,15 @@ struct ItemPreview: View {
             onSetup: { result in
                 guard case .success(let store) = result else { return }
 
+                // let item = Item()
+                // let date = Calendar.current.date(from: DateComponents(year: 2025, month: 11, day: 25, hour: 4, minute: 26))!
+                // item.name = "Scheduled"
+                // item.blockMode = .schedule
+                // item.scheduleWindow = ScheduleWindow()
+                // item.scheduleWindow.start = date
+                // item.scheduleWindow.end = date.addingTimeInterval(TimeInterval(60 * 5))
+                // store.mainContext.insert(item)
+
                 let items: [(String, BlockMode)] = [
                     ("Timer", BlockMode.timer),
                     ("Schedule", BlockMode.schedule),
@@ -173,7 +213,6 @@ struct ItemPreview: View {
                     let item = Item()
                     item.name = name
                     item.blockMode = mode
-
                     store.mainContext.insert(item)
                 }
             })
